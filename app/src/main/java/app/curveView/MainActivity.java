f5d370a6fdffa51973ce4d2d8a7de3ef2ea21c0d
Akash Jain <akash@appinventiv.com>
2017-11-29 17:06:47 +0530

    initialize repo

---
+++ app/src/main/java/app/curveView/MainActivity.java
@@ -0,0 +1,23 @@
+package app.curveView;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(developer.shivam.crescento.R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView()
+                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+}
